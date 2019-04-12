758693484228b747ae4aa305bcca80dfc136279e
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-12 15:46:34 +0200

    use cobalt.plugins.send(this,action,data,callback)

--- cobalt.pdf.js
+++ cobalt.pdf.js
@@ -6,21 +6,13 @@
             cobalt.openpdf = this.openpdf.bind(this);
         },
         openpdf: function(data, callback) {
-            this.send("pdf", data, function(data) {
+            cobalt.plugins.send(this, "pdf", data, function(data) {
                 if (typeof callback == 'function') {
                     callback(data);
                 }
             })
         },
-        handleEvent: function(json) {},
-        send: function(action, data, callback) {
-            cobalt.send({
-                type: "plugin",
-                name: this.name,
-                action: action,
-                data: data,
-            }, callback);
-        }
+        handleEvent: function(json) {}
     };
     cobalt.plugins.register(plugin);
 })(cobalt || {});
