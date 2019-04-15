15c7f131c4d7dfc7f35271301e807d60e633d7c5
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-15 10:19:24 +0200

    ready to handle event

--- cobalt.pdf.js
+++ cobalt.pdf.js
@@ -1,17 +1,22 @@
 (function(cobalt) {
-    var plugin = {
-        classes: {
-        	ios: 'CobaltPdfPlugin',
-			android: 'io.kristal.pdfplugin.PdfPlugin'
-        },
-        init: function() {
-            //create shortcuts
-            cobalt.openpdf = this.openpdf.bind(this);
-        },
-        openpdf: function(data, callback) {
-            cobalt.plugins.send(this, "pdf", data, callback)
-        }
-    };
-    cobalt.plugins.register(plugin);
+  var plugin = {
+    classes: {
+      ios: 'CobaltPdfPlugin',
+      android: 'io.kristal.pdfplugin.PdfPlugin'
+    },
+    init: function() {
+      cobalt.openpdf = this.openpdf.bind(this);
+    },
+    openpdf: function(data, callback) {
+      cobalt.openpdfCallback = callback;
+      cobalt.plugins.send(this, "pdf", data)
+    },
+    handleEvent: function(json) {
+      if (typeof cobalt.openpdfCallback === 'function') {
+        cobalt.openpdfCallback(json.data)
+      }
+    }
+  };
+  cobalt.plugins.register(plugin);
 })(cobalt || {});
 
