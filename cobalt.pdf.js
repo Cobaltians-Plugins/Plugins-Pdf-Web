c08c51a35f5773ad3c216d75d3447419da03036d
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-16 16:39:38 +0200

    Update cobalt.pdf.js

--- cobalt.pdf.js
+++ cobalt.pdf.js
@@ -9,13 +9,7 @@
       cobalt.openpdf = this.openpdf.bind(this);
     },
     openpdf: function(data, callback) {
-      this.openpdfCallback = callback;
       cobalt.plugins.send(this, "pdf", data)
-    },
-    handleEvent: function(json) {
-      if (typeof this.openpdfCallback === 'function') {
-        this.openpdfCallback(json.data)
-      }
     }
   };
   cobalt.plugins.register(plugin);
